abf6a5417fc197b240e9c75e4c7f4e0626faf9db
ARYANJAKKAL123 <aryanjakkal717@gmail.com>
2026-02-05 18:18:10 +0530

    DAY2: Basic Honeypot server capturing IP and PATH

---
+++ server.py
@@ -0,0 +1,23 @@
+print("SERVER FILE STARTED")
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class HoneypotServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        ip = self.client_address[0]
+        path = self.path
+
+        print("IP:", ip)
+        print("Requested Path:", path)
+        print("---------------------")
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Living Honeypot Active")
+
+server = HTTPServer(("localhost", 8000), HoneypotServer)
+
+print("Honeypot running on http://localhost:8000")
+
+server.serve_forever()
